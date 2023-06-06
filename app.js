4cbcb73f03fd2ccb800f3916f8e567487479791c
StraightforwardDigital <111469465+StraightforwardDigital@users.noreply.github.com>
2023-06-05 21:53:19 -0300

    adding js file

---
+++ app.js
@@ -0,0 +1,14 @@
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    //designate the first element in array to variable
+    let smallest = args[0];
+    //run a for loop checking to see if smallest is smaller than next variable in array
+    for (let i = 1; i < args.length; i++){
+      if (args[i] < smallest) {
+        smallest = args[i];
+      }
+    }
+  //return the smallest variable
+  return smallest;
+  }
+}
